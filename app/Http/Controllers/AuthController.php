eadbe7be37c7d0b90b8f5a0dea5cdd165d3ea833
Bas <bas@icweb.co.th>
2021-09-10 10:49:48 +0700

    start

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+   public function register(Request $request){
+       $fields = $request->validate([
+            'fullname' => 'required|string',
+            'username' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+            'tel' => 'required',
+            'role' => 'required|integer',
+       ]);
+
+       $user = User::create([
+        'fullname' => $fields['fullname'],
+        'username' => $fields['username'],
+        'email' => $fields['email'],
+        'password' => bcrypt($fields['password']),
+        'tel' => $fields['tel'],
+        'role' => $fields['role'],
+       ]);
+       
+        $token =  $user->createToken($request->userAgent(),["$user->role"])->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+        return response($response,201);
+   }
+
+   public function login(Request $request){
+    $fields = $request->validate([
+        'email' => 'required|string',
+        'password' => 'required|string',
+   ]);
+
+   
+   
+    $user =  User::where('email',$fields['email'])->first();
+    if(!$user || !Hash::check($fields['password'], $user->password)){
+        $response = [
+            'message' => 'email & password false',
+        ];
+        return response($response,203);
+    }else{
+        $user->tokens()->delete();
+        $token =  $user->createToken($request->userAgent(),["$user->role"])->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+        return response($response,201);
+    }
+   }
+
+   public function logout(Request $request){
+
+    auth()->user()->tokens()->delete();
+    return [
+        'message' => 'loged out'
+    ];
+
+   }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = auth()->user();
+
+        if($user->tokenCan('1')){
+        $request->validate([
+            'name' => 'required|min:5',
+            'slug' => 'required',
+            'price' => 'required',
+            
+        ]);
+        return Product::create($request->all());
+        }else{
+            return [
+                'status'=>'permission denied'
+            ];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Product::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request  $request,$id)
+    {
+        //
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $request;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        return Product::destroy($id);
+    }
+}
